350bb76a59d8ace86df554aeb8143abaaf35c126
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-11 00:46:41 +0300

    Programming-Languages

---
+++ Python-Programming/# Print - Type - Yorum Satırı Kullanımı.py
@@ -0,0 +1,23 @@
+#yorum satırı yapma
+
+"""
+Birden 
+fazla 
+satirin 
+yorumlanmasi
+"""
+print("Merhaba Ben Enes")
+
+type(9) # int değer
+
+type("12") # str değer
+
+c ="a" + "b" # ab çıktısı verilir 
+d = "a" "b" # ab çıktısı verilir
+e = "a " "b" # a b çıktısı verilir
+f = "a" * 3 # aaa çıktısı verili
+
+print("==>>",c,d,e,f)
+
+
+
