f6ac9906f52ecd2bbf245905cc3a370803b823a8
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-12 00:12:39 +0300

    Programming-Languages

---
+++ C-Programlama-Turkish/A 1 (bir alt satıra geçme - printf - yorum satırı)/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() // program main fonksiyonunun içini algýlar. Ýçine ne yazdýysak o çalýþýr.
+{
+    printf("    Enes  \n"); // \n bir alt satýra geçme komutudur
+    printf("    Berke \n");
+    printf("    Karaoglan \n");
+
+
+    printf("Enes\nBerke\nKaraoglan"); // yukarýdaki örnek ile ayný sonucu teþkil etmektedir
+
+
+    return 0;  //olmasa da olur program baþarýlý bir þekilde çalýþýyoru göstermek için kullanýlýr
+}
+
+
+
+// birden çok kýsmý yorum satýrýnýn içine almak için aþaðýdaki örnek yapýlýr
+
+/*
+ENES
+BERKE
+KARAOÐLAN
+MERHABA
+DÜNYA */
+
+
+
+
+
+
+
+
+
