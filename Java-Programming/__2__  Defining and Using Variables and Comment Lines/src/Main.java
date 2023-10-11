562afc0cdf456ac8389e9134612600c4e8133093
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-12 00:23:02 +0300

    Programming-Languages

---
+++ Java-Programming/__2__  Defining and Using Variables and Comment Lines/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int deger = 125;
+        System.out.println("Girilen deger ==> " + deger);
+
+        int a = 5;
+
+        int b = a * 7;
+
+        int c = a + b;
+
+        int toplam;
+
+        toplam = a + b + c;
+
+        System.out.println("İslem sonucu ==> " + toplam);
+    }
+}
