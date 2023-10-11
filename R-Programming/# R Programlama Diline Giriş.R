6e7f7bd308aca59844d5b866edfd69b4e898bfd7
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-11 22:38:40 +0300

    Programming-Languages

---
+++ R-Programming/# R Programlama Diline Giriş.R
@@ -0,0 +1,93 @@
+# R Programlama Diline Giriş
+--------------------------------------------------------------------------------
+var3 = 12
+var2 = 45
+var1 = 11
+var4="R Programming"
+
+--------------------------------------------------------------------------------
+
+# İşlemler
+
+12 * 33
+
+13 + 54
+
+x = 33
+y = 56
+
+sonuç = y - x
+print(sonuç)
+
+--------------------------------------------------------------------------------
+
+# Üs Alma İşlemi
+
+üs_alma = 5 ^ 2
+print(üs_alma)
+
+--------------------------------------------------------------------------------
+
+# Logaritma Alma İşlemi
+
+logaritma = 25
+print(log(logaritma))
+
+logaritma_2 = 16
+print(log2(logaritma_2))
+
+--------------------------------------------------------------------------------
+
+# Mutlak Değer Alma İşlemi
+
+negatif_deger = -46
+print(abs(negatif_deger))
+
+--------------------------------------------------------------------------------
+
+# Hafızadaki Değişkenleri Kontrol Etme
+
+ls()
+
+--------------------------------------------------------------------------------
+  
+# Hafızadaki Değişkeni Silme
+
+silinecek_deger = 999
+rm(silinecek_deger)
+ls()
+
+# rm(list=ls()) Bütün değişkenler bir listede toplanır ve silinir.
+
+--------------------------------------------------------------------------------
+# İşlemler Ve Operatörler
+
+"""
+Mod Alma ==> %
+Üs Alma ==> ^
+Parantez ==> ()
+Mutlak Değer ==> abs()
+Faktoriyel ==> factorial()
+Sayı İşaretini Görme ==> sign()
+e üzeri ==> exp()
+10 Tabanında Logaritma ==> log()
+2 Tabanında Logaritma ==> log2()
+Kök Alma ==> sqrt()
+Sinüs ==> sin()
+Kosinüs ==> cos()
+Tanjant ==> tan()
+Yukarı Tamsayıya Yuvarlama ==> ceiling()
+Aşağı Tamsayıya Yuvarlama ==> floor()
+En Yakın Tamsayıya Yuvarlama ==> round()
+Gerçel Sayının Tamsayı Kısmı ==> trunc()
+Gerçel Sayının Basamak Sayısına İndirgeme ==> signif()
+
+
+Elemanıdır ==> %in%
+
+"""
+
+
+
+
+
