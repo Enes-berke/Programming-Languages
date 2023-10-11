c9b7a949e70c2296ea80805e5ca910179853a7ed
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-12 00:14:11 +0300

    Programming-Languages

---
+++ C-Sharp-Programming-English/#1 Console.Write - Console.WriteLine/#1 Console.Write - Console.WriteLine/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Gıthub :D ");
+Console.WriteLine("Enes Berke Karaoglan");
+
+
+
+Console.Write("Example 1 ");
+Console.Write("- Example 2");
