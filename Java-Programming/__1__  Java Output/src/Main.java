42657410a3d8d09b4e700d0c21f80d7ec687f4c3
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-12 00:12:14 +0300

    Programming-Languages

---
+++ Java-Programming/__1__  Java Output/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args){
+        //Java Output Text / Print
+        System.out.println("Hello GıtHub");
+
+       //Java Output Numbers
+        System.out.println(1888);
+
+        System.out.println(3+9);
+        System.out.println(12*56);
+
+
+        //System.out.println();==> sout yazildiğinde "System.out.println();" ifadesi yazdirilir
+    }
+}
+// public class Main {
+
+//    public static void main(String[] args) { # Main Metodu #
+
+//        System.out.println("Hello World");
+//    }
+//}
