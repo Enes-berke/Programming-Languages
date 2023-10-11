e8a761d8858d2c9a845ef44925d134f77ef450b6
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-12 00:20:13 +0300

    Programming-Languages

---
+++ C-Sharp-Programming-English/#2 Console.ReadLine/#2 Console.ReadLine/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Enes Berke Karaoğlan");
+Console.ReadLine();
+// Console.ReadLine kullanıcıdan değer girilmesini bekler. Bu sebeple açılan konsolun hemen açılıp kapanmasının önüne geçilmiş olur.
+// Kullnaıcı bir değer girip enter tuşuna basana kadar bekler
+// Bir değer girilip enter tuşuna basıldıktan sonra bir alt satıra geçer ve uygulamayı sonlandırır.
