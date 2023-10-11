dec5a2f6c2c1fada29288b71097a6a2c3cd1f9cd
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-12 00:13:00 +0300

    Programming-Languages

---
+++ CPP-Programming/a.1 # Giriş Dersi (Console - New Lines)/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "C++ Giris Dersi";
+
+    cout << "\n" ; // New Lines
+
+    cout << "Enes Berke Karaoglan \n"; // New Lines
+
+    cout << "Hello World!" << endl; //
+    return 0;
+}
