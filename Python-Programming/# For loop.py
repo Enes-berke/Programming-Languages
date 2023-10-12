fb83c8c4f1dedfeb1c88e36baf7b788b35a7186d
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-13 00:46:53 +0300

    Programming-Languages

---
+++ Python-Programming/# For loop.py
@@ -0,0 +1,20 @@
+# For loop
+    
+for each in range(1,11):
+    print(each)
+    
+for each in "ankara ist":
+    print(each)
+    
+for each in "ankara ist".split(): 
+    print(each)
+    
+liste = [1,4,5,6,8,3,3,4,67]
+ 
+summation = sum(liste)    
+
+count = 0
+for each in liste:
+    count = count + each
+    print(count)
+    
