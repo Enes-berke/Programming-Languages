f4a1fe0e4857cb5e7dfa8ecc434bf2a36ace4173
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-13 00:46:27 +0300

    Programming-Languages

---
+++ Python-Programming/# String Metodları - replace().py
@@ -0,0 +1,11 @@
+# String Metodları - replace()
+
+# İstenilen karakteri istenilen karakter ile değiştirir.
+
+# Replaces the desired character with the specified character.
+
+degisişim = "gelecegi_yazanlar"
+
+print(degisişim.replace("e","a"))
+
+print(degisişim.replace("a","i"))
