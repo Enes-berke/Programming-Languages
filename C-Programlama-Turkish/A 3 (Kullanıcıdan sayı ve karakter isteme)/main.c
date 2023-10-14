4a30c373ed50d5060eb4495ebe5326f6c8462333
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-14 14:03:27 +0300

    Programming-Languages

---
+++ C-Programlama-Turkish/A 3 (Kullanıcıdan sayı ve karakter isteme)/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int sayideger ;
+
+    printf("Lutfen Bir Deger Giriniz => \n");
+    scanf(" %d",&sayideger);
+
+    printf(" Girdiginiz Sayi Degeri => %d", sayideger);
+
+
+    char karakter ;
+
+    printf("\n Lutfen Bir Karakter Giriniz => \n");
+    scanf(" %c",&karakter);
+
+    printf(" Girdiginiz Karakter Degeri => %c", karakter);
+
+
+    return 0;
+}
+
+
