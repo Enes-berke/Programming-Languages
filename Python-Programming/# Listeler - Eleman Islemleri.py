7078478171cc8b64ed0e016ba1ca73c972ab4bfe
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-15 12:55:24 +0300

    Programming-Languages

---
+++ Python-Programming/# Listeler - Eleman Islemleri.py
@@ -0,0 +1,23 @@
+#Listeler - Eleman Islemleri
+
+
+liste = [10,20,30,40,50]
+
+print(liste[0])
+print(liste[1])
+
+print(liste[6])
+
+print(liste[0:2])
+
+print(liste[:2])
+
+print(liste[2:])
+
+yeni_liste = ["a",10,[20,30,40,50]]
+
+print(yeni_liste[2])
+
+print(yeni_liste[0:2])
+
+print(yeni_liste[2][1])
