6b52c36fc6ac8062151448c931668354eb2dbea7
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-15 12:57:33 +0300

    Programming-Languages

---
+++ JavaScript-Programming/# 3 x 3 Matris Oluşturma.js
@@ -0,0 +1,15 @@
+// # 3 x 3 Matris Oluşturma
+
+var i, k, t ; 
+
+var x = 1
+        for(t=0 ; t<3 ; t++){
+
+            for(k=0 ; k<3 ; k++){
+                document.write(x + " ")
+                x = x+1
+
+
+                }
+                document.write("<br>")
+            }        
