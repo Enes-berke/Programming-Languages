412236e57e6a155d036679ea8222865d1180e4c1
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-15 12:53:34 +0300

    Programming-Languages

---
+++ C-Programming-English/A 4 (Arithmetic Operators)/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+/*
+    Arithmetic Operator
+    +  -  *  /  %  ++  --
+
+    */
+    int x = 15;
+    int y = 10;
+    int sonuca;
+    int sonucb;
+    int sonucc;
+    int sonucd;
+    int sonuce;
+
+
+    sonuca = x - y;
+    sonucb = x + y;
+    sonucc = x / y;
+    sonucd = x * y;
+    sonuce = x % y;
+
+    printf(" => %d \n",sonuca);
+    printf(" => %d \n",sonucb);
+    printf(" => %d \n",sonucc);
+    printf(" => %d \n",sonucd);
+    printf(" => %d \n",sonuce);
+    printf(" => %d \n",x-5);
+
+
+
+
+    return 0;
+}
