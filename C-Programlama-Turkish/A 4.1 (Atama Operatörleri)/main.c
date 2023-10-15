a3fe8e3f3493f162062a8187f745366d3ee47a72
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-15 12:52:42 +0300

    Programming-Languages

---
+++ C-Programlama-Turkish/A 4.1 (Atama Operatörleri)/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    /*
+    Assignment Operator
+    = += -= *= /= %=
+
+    */
+
+    int x = 25;
+    //x = x+3;
+    //x += 3;
+   // x = x*3;
+    //x *= 3;
+
+    //x = x/5;
+    //x /= 5 ;
+
+    //x=x%6;
+    x %=6;
+
+    printf("%d",x);
+
+
+
+
+
+
+    return 0;
+}
