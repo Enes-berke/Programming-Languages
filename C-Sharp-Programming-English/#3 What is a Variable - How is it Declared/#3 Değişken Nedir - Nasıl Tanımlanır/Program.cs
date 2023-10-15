9a4a2e46c015b84da6a79380888bc9388ffc0659
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-15 13:01:56 +0300

    Programming-Languages

---
+++ C-Sharp-Programming-English/#3 What is a Variable - How is it Declared/#3 Değişken Nedir - Nasıl Tanımlanır/Program.cs
@@ -0,0 +1,13 @@
+﻿//class Program {
+   // static void main(String[] args)
+   // {
+
+        // değişkenTipi değişkenAdi = deger ;   
+
+        String NameLastName = "Enes Berke Karaoğlan" ;
+        Console.WriteLine(NameLastName);
+
+   // }
+
+
+//}
