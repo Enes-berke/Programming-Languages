8f7983ba3f9315ae35718e505deb138ddf93f7ac
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-15 12:55:03 +0300

    Programming-Languages

---
+++ Python-Programming/# Liste - 1.py
@@ -0,0 +1,22 @@
+# Liste - 1
+
+liste = [1,2,3,4,5,6]
+type(liste)
+
+liste_str = ["pazartesi","sali","carsamba"]
+type(liste_str)
+
+value = liste[1]
+print(value)
+
+last_value = liste[-1] # son elemanın seçilmesi
+
+liste_divide = liste[0:3] # 0 - 1 - 2 indeksteki elemanlar 
+
+liste.append(7) # 7 elemanı listeye eklenir
+liste.remove(7) # 7 elemanı listeden silinir
+liste.reverse() # liste ters çevrilir
+liste2 = [1,5,4,3,6,7,2]
+liste2.sort()   # listedeki elemanları küçükten büyüğe doğru sıralar
+
+string_int_liste = [1 , "ee" , 2 , "cc" , 3 ,"aa" , "bb"]
