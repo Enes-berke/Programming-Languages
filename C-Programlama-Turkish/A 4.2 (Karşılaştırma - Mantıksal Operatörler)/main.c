e65781870b245846f08acb365bd332a3ecd630a2
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-15 12:52:46 +0300

    Programming-Languages

---
+++ C-Programlama-Turkish/A 4.2 (Karşılaştırma - Mantıksal Operatörler)/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    /*
+    Comparison Operators
+    ==  !=  >  <   >=  <=
+
+    */
+
+    /*
+    int x=25;
+    int y =20;
+
+    printf("%d",x >= y);*/
+
+
+     /*
+    Logical Operators
+    && || !
+
+    */
+
+    int x=25;
+    int y =20;
+
+    printf("%d",!(x >10 || x<20) );  // x 20 den küçük veya 10 dan büyük mü = evet >>> ! iþaretinden evet hayýra dönüyor sonuç 0 oluyor
+
+    /*
+
+    || ===> veya anlamýna gelmektedir.
+    && ===> ve anlamýna gelmektedir.
+    !  ===> değil iþlenen sıfır ise doðru(true) çýktý alýr.
+
+    */
+
+
+
+
+
+
+
+    return 0;
+
+
+
+
+
+    return 0;
+    }
