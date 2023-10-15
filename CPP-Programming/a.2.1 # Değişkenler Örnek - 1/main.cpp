c9e5513b0c823a65850e235e7230d2a3a59140c8
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-15 12:56:17 +0300

    Programming-Languages

---
+++ CPP-Programming/a.2.1 # Değişkenler Örnek - 1/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int myAge = 35;
+    cout << "I am " << myAge << " years old.";
+
+
+    int a = 5 , b = 12 ;
+
+    cout << endl << "a = > " << a << endl << "b = > " << b ;
+
+    cout << endl << "a + b = " << a + b << endl ;
+    cout << "a * b = " << a * b ;
+
+    return 0;
+}
