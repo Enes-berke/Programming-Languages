d34f183ca807d84aa5cffe4c72c587c76340ec83
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-15 12:56:32 +0300

    Programming-Languages

---
+++ CPP-Programming/a.2.2 # Değişkenler Örnek - 2/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string kelime1 = "Enes " , kelime2  ="Berke" , isim = kelime1 + kelime2 ;
+    cout << isim ;
+
+    string kelime3 = "Hello " , kelime4 = "GitHub" , sonuc1 = kelime3 + kelime4 ;
+    cout << endl << sonuc1 ;
+
+
+    int myAge = 19;
+    cout << endl << "I am " << myAge << " years old.";
+
+
+    return 0;
+}
