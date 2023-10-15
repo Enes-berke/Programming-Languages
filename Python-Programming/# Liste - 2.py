39328284287f32ad3e24d5bb267c870dd5818fde
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-15 12:55:06 +0300

    Programming-Languages

---
+++ Python-Programming/# Liste - 2.py
@@ -0,0 +1,23 @@
+#Liste - 2 
+
+#[]
+#list()
+
+notlar = [90,80,70,50]
+print(type(notlar))
+
+liste = ["a",19.3,90]
+list_genis = ["a",19.3,90, notlar]
+
+print(len(list_genis))
+
+print(list_genis[0])
+print(list_genis[1])
+print(list_genis[3])
+
+print(type(list_genis[3]))
+
+tum_liste = [liste, list_genis]
+
+#del tum_liste ==> liste silinmiş olur.
+
