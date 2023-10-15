208036ce81fdf182d5aa456ebcb2ad969b9840fd
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-15 12:55:22 +0300

    Programming-Languages

---
+++ Python-Programming/# Listeler - Eleman Degistirme.py
@@ -0,0 +1,25 @@
+#Listeler - Eleman Degistirme
+
+liste = ["ali","veli","berkcan","ayse"]
+
+print(liste)
+
+liste[1] = "velinin_babasi"
+
+print(liste)
+
+liste[1] = "veli"
+
+liste[0:3] = "alinin_babasi","velinin_babasi","berkcanin_babasi"  
+
+print(liste)
+
+liste = ["ali","veli","berkcan","ayse"]
+
+print(liste)
+
+
+liste = liste + ["kemal"]
+
+print(liste)
+
