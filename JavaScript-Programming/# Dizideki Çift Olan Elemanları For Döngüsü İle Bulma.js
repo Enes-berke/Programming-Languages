ce0faa96505d38aa4bce7f7c6b116b0509725b1d
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-16 23:14:27 +0300

    Programming-Languages

---
+++ JavaScript-Programming/# Dizideki Çift Olan Elemanları For Döngüsü İle Bulma.js
@@ -0,0 +1,12 @@
+// # Dizideki Çift Olan Elemanları For Döngüsü İle Bulma
+
+var i ;
+            var deneme = [2,3,4,5,6,7,8,998,68,45,24,56,678]
+
+            for(i=0 ; i<deneme.length ; i++){
+
+                if(deneme[i] % 2 == 0 )
+                document.write(deneme[i] + "<br>")
+
+
+            }
