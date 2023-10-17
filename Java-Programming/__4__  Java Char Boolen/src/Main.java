cf18bafaab0b59c078cfb046b99aaf25b81a71a6
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-17 21:56:57 +0300

    Programming-Languages

---
+++ Java-Programming/__4__  Java Char Boolen/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Char Veri Tipi : Karakterleri göstermek için kullanılır. 2 byte yer kaplar. 2^16 tane karakter simgelenebilir.
+        // Boolean Veri Tipi : Koşul Durumlarında kullanılır. true veya false değeri alır.
+
+        boolean a = true;
+        boolean b = false;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
