4e2d2fac99a033d2b0a4fc1c74221b352c5ae03b
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-17 21:57:59 +0300

    Programming-Languages

---
+++ Python-Programming/# While loop.py
@@ -0,0 +1,16 @@
+# While loop
+    
+i = 0
+while(i <4):
+    print(i)
+    i = i + 1
+
+liste = [1,4,5,6,8,3,3,4,67]
+
+
+sinir = len(liste)   
+each = 0
+count = 0
+while(each < sinir):
+    count = count + liste[each]
+    each = each + 1 
