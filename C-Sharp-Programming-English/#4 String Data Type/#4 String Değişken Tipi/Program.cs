d34dc9b357bcd0da7fdcb77ad4051fa427af4d92
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-17 21:55:20 +0300

    Programming-Languages

---
+++ C-Sharp-Programming-English/#4 String Data Type/#4 String Değişken Tipi/Program.cs
@@ -0,0 +1,14 @@
+﻿
+// String Tipi 
+
+String NameLastName = "Enes Berke Karaoğlan";
+Console.WriteLine(NameLastName);
+
+String buyukKarakter = NameLastName.ToUpper(); // Tüm harfler büyük şekilde yazılır.
+Console.WriteLine(buyukKarakter);
+
+String kucukKarakter = NameLastName.ToLower(); // Tüm harfler küçük şekilde yazılır.
+Console.WriteLine(kucukKarakter);
+
+String BelirliBirbolum = NameLastName.Substring(3, 7); // 3. karakterden vaşla ve 7 karakter al anlamına geliyor.
+Console.WriteLine(BelirliBirbolum);
