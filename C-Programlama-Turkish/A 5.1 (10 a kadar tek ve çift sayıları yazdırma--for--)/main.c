308806673af3c3cbc12f51e73082de20d5674a89
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-18 20:48:33 +0300

    Programming-Languages

---
+++ C-Programlama-Turkish/A 5.1 (10 a kadar tek ve çift sayıları yazdırma--for--)/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int  i ;
+
+
+    for(i=0 ; i<=10 ; i++){
+
+            if(i%2!=0) // i%i == 0 dersek sadece çift sayýlarý yazdýrýr
+            {
+                printf(" %d\n",i);
+            }
+
+    }
+
+
+
+
+    return 0;
+}
+
+
