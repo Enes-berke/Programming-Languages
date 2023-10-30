3545f96c183c2854dea81856a5840a4cb5b0fcbc
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-30 22:54:59 +0300

    Programming-Languages

---
+++ C-Programlama-Turkish/AB 15 (İki boyutlu sayı dizileri)/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+
+{
+
+    int dizi[2][3] =   {{11,22,44},  // [0][-]
+                        {55,66,77}}; // [1][-]
+    // [satýr][sütun]
+
+    printf("%d",dizi[1][2]);
+    return 0;
+}
