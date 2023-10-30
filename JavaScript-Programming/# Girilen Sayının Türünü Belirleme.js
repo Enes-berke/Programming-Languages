bd2e9393b9788b3f859ae45b8509f924f6ee1ad2
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-30 22:48:38 +0300

    Programming-Languages

---
+++ JavaScript-Programming/# Girilen Sayının Türünü Belirleme.js
@@ -0,0 +1,19 @@
+// # Girilen Sayının Türünü Belirleme
+
+var sayi = prompt("bir sayı değeri giriniz = ");
+
+if (sayi % 2 == 1 && sayi > 0)
+
+{
+
+document.write("sayi pozitif tek sayıdır.");
+
+   }
+
+   else{
+
+    document.write("sayı tek sayı değil veya pozitif değil!!")
+
+   }
+
+
