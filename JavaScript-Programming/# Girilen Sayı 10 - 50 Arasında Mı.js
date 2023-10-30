987d82ae7e4d7d59b90bb1f532c7e46321169b98
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-30 22:48:31 +0300

    Programming-Languages

---
+++ JavaScript-Programming/# Girilen Sayı 10 - 50 Arasında Mı.js
@@ -0,0 +1,13 @@
+// # Girilen Sayı 10 - 50 Arasında Mı ? 
+
+var sayi1 = prompt("bir sayı değeri giriniz =");
+
+if (sayi1>10 && sayi1<50) {
+
+   document.write("sayı 10-50 arasındadır.");
+
+   } else {
+
+   document.write("sayı 10-50 arasında değildir.");
+
+   }
