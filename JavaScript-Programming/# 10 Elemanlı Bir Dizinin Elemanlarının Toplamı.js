5d3dccf44cd56cdddbfdfdc0ad4ec744e72aa69f
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-30 22:48:05 +0300

    Programming-Languages

---
+++ JavaScript-Programming/# 10 Elemanlı Bir Dizinin Elemanlarının Toplamı.js
@@ -0,0 +1,15 @@
+// # 10 Elemanlı Bir Dizinin Elemanlarının Toplamı
+
+var a = [1,2,3,4,5,6,7,8,9,10];
+var i, toplam;
+toplam = 0 ;
+
+for(i = 0 ; i <10 ; i++){
+
+    toplam +=  a[i] ;
+
+
+
+}
+
+document.write(toplam);
