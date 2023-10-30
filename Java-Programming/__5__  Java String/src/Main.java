783c0c6d2654bd92627372aacb7841f0630c9f6f
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-30 22:47:47 +0300

    Programming-Languages

---
+++ Java-Programming/__5__  Java String/src/Main.java
@@ -0,0 +1,40 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        String t = "Hello GitHub" ;
+        System.out.println(t);
+
+        String x = "Java " ;
+        String y = "Programming" ;
+        System.out.println( x + y );
+
+        String a = "Enes " ;
+        String b = "Berke " ;
+        String c = "Karaoglan" ;
+        System.out.println( a + b + c);
+
+        String e = "Yas";
+        int e2 = 19 ;
+        System.out.println( e + ":" + e2) ;
+
+        String a1 = "Hello, ";
+        String a2 = "How are you";
+        char a3 = '?' ;
+        String a4 = a1 + a2 + a3 ;
+        System.out.println(a4);
+
+
+        String a5 = "Enes"
+                    + " Berke"
+                    + " Karaoglan" ;
+        System.out.println(a5);
+
+        // Boşuk ==> \n
+
+        String a6 = " Name = Enes Berke \n Last Name = Karaoglan \n Location = Turkey " ;
+        System.out.println(a6);
+    }
+
+}
