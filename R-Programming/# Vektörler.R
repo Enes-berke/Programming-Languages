0281b32163b9a844c14ff73a19021633dcd8e073
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-30 22:50:47 +0300

    Programming-Languages

---
+++ R-Programming/# Vektörler.R
@@ -0,0 +1,51 @@
+# Vektörler
+--------------------------------------------------------------------------------
+var1 = 43  
+
+var2 = c(1,2,3,4,5,6,7)
+
+var3 = c("Yes" , "No")
+
+var4 = c(7:14)
+
+var5 = c(1.5:20.3)
+
+var6 = seq(from = 7 , to = 20 , by = 3)  # Başlangıç , bitiş , artış miktarı
+
+var7 = rep(10,3) # rep() tekrarlama için kullanılır. ==> 10 sayısı 3 kere tekrarlansın
+
+var8 = rep("Enes Berke" , 7)
+
+var9 = rep(7 : 12  ,  2) # 7 den 12 ye kadar sayıları 2 kez tekrar ettir anlamına geliyor.
+
+var10 = rep( c( 11 ,12 , 13) , c( 1 , 2 , 3) ) # 11 => 1 kere 12 => 2 kere ve 13 => 3 kere tekrar edilsin
+--------------------------------------------------------------------------------
+
+print(var1)
+
+print(var2)
+
+print(var3)
+
+print(var4)
+
+print(var5)
+
+print(var6)
+
+print(var7)
+
+print(var8)
+
+print(var9)
+
+print(var10)
+
+
+
+
+
+
+
+
+
