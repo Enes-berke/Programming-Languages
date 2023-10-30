244015aeebb1295b0228f984f848e4c250d10c0c
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-30 22:54:47 +0300

    Programming-Languages

---
+++ C-Programlama-Turkish/AB 14 (Tek boyutlu sayı dizileri)/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+   int i , dizi[10];
+   int a = 0;
+   for( i=0 ; i<10 ; i++)
+   {
+       dizi[a]=i;
+       printf("%d\n",dizi[a]);
+       a++;
+   }
+}
