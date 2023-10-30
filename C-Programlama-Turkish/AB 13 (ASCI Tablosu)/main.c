aff7d6afbedf305ea04f740dd921e2cb58261e82
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-30 22:54:38 +0300

    Programming-Languages

---
+++ C-Programlama-Turkish/AB 13 (ASCI Tablosu)/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("%d\t  %d\t  %d\t",'a','b','c'); // bu þekilde a b c nin ASCI tablosundaki deðeri
+                                            // consola yazdýrýlmýþ olur.
+    return 0;
+}
+
+
+// \t demek bir TAB kadar boþluk býrak demek
